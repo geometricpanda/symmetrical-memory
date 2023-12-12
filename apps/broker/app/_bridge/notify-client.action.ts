a5c0339e94c48dc21a47cc28489d948ff531490f
Jim Drury <jim.drury@virginmediao2.co.uk>
2023-12-12 09:26:49 +0000

    feat: updated google pubsub

--- apps/broker/app/_bridge/notify-client.action.ts
+++ apps/broker/app/_bridge/notify-client.action.ts
@@ -2,9 +2,38 @@
 
 import 'server-only';
 import { ISbStoryData } from '@storyblok/react';
+import { PubSub } from '@google-cloud/pubsub';
 
-export const notifyClient = (storyId: string, deviceId: string, story: ISbStoryData) => {
+const pubSubClient = new PubSub();
 
+const { GOOGLE_TOPIC_NAME } = process.env;
 
-
+if (!GOOGLE_TOPIC_NAME) {
+  throw new Error('Missing GOOGLE_TOPIC_NAME');
 }
+
+interface Message {
+  ClientId: string;
+  StoryData: string;
+  StoryId: number;
+}
+
+export const notifyClient = async (storyId: string, deviceId: string, story: ISbStoryData) => {
+
+  const message: Message = {
+    ClientId: deviceId,
+    StoryData: JSON.stringify(story),
+    StoryId: +storyId
+  };
+
+  const dataBuffer = Buffer.from(JSON.stringify(message));
+
+  try {
+    await pubSubClient
+      .topic(GOOGLE_TOPIC_NAME)
+      .publishMessage({ data: dataBuffer });
+  } catch (error) {
+    console.error(`Received error while publishing:`, error);
+  }
+
+};
